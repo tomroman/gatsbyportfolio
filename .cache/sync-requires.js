b31886e4ffd6550d78d53746a086b3d2635978a8
Thomas Roman <thomasroman32@yahoo.com>
2019-05-02 23:54:07 -0500

    taking some projects off to work on visual components

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,12 +5,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-project-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/templates/project.js"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/about.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/page-2.js"))),
-  "component---src-pages-projects-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/projects.js"))),
-  "component---src-templates-project-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/templates/project.js")))
+  "component---src-pages-projects-js": hot(preferDefault(require("/Users/thomasroman/Desktop/gatsbyportfolio/src/pages/projects.js")))
 }
 
