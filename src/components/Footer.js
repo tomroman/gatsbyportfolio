87e18836b598f33c06a5347db1ec118dc6d8fe89
Thomas Roman <thomasroman32@yahoo.com>
2019-05-11 23:36:45 -0500

    footer component is displaying on all pages

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <div className="inner">
+                    <ul className="icons">
+                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                    </ul>
+                    <ul className="copyright">
+                        <li>&copy; Gatsby</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,7 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Menu from "../components/menu"
 import "./layout.css"
+import Footer from "../components/Footer"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -37,11 +38,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <Footer />
         </div>
       </>
     )}
