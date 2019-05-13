a9a2da0ab665a9d930d874080fd3c87f55cae5e0
Thomas Roman <thomasroman32@yahoo.com>
2019-05-12 23:54:47 -0500

    removed import footer from header to avoid error when rendering

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Footer from "../components/Footer"
+
 
 const Header = ({ siteTitle }) => (
   <header
