362c2631b77535de1bbb6529f15f0dc11533f436
Thomas Roman <thomasroman32@yahoo.com>
2019-05-20 23:55:34 -0500

    commented out footer links only leaving copyright links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 class Footer extends React.Component {
     render() {
         return (
             <div id="footer">
                 <div className="inner">
-                    <ul className="icons">
+                    {/* <ul className="icons">
                         <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
+ 
+                    </ul> */}
                     <ul className="copyright">
                         <li>&copy; Gatsby</li>
                     </ul>
