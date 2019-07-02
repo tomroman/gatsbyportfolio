a06174fd8e18c0ac9fdc3225189408612fd7545a
Thomas Roman <thomasroman32@yahoo.com>
2019-07-01 23:54:45 -0500

    commented out the contact.js page in order to allow the project to render on localhost. Will add back at a later time when I can have it without causing an error.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,99 +1,99 @@
 
-import React from "react";
-import styled from "styled-components";
-import "./normalize.css";
-import Layout from "../templates/layout";
-import GeneralForm from "../components/GeneralForm";
-import ProjectForm from "../components/ProjectForm";
+// import React from "react";
+// import styled from "styled-components";
+// import "./normalize.css";
+// import Layout from "../templates/layout";
+// import GeneralForm from "../components/GeneralForm";
+// import ProjectForm from "../components/ProjectForm";
 
 
 
-class Contact extends React.Component {
-    constructor(props) {
-      super(props);
-      this.showGenForm = this.showGenForm.bind(this);
-      this.hideGenForm = this.hideGenForm.bind(this);
-      this.showProjectForm = this.showProjectForm.bind(this);
-      this.hideProjectForm = this.hideProjectForm.bind(this);
+// class Contact extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.showGenForm = this.showGenForm.bind(this);
+//       this.hideGenForm = this.hideGenForm.bind(this);
+//       this.showProjectForm = this.showProjectForm.bind(this);
+//       this.hideProjectForm = this.hideProjectForm.bind(this);
   
-      this.state = {
-        genFormOpen: false,
-        proFormOpen: false
-      };
-    }
+//       this.state = {
+//         genFormOpen: false,
+//         proFormOpen: false
+//       };
+//     }
   
-    showGenForm() {
-      //if other form is open, close it first
-      if (this.state.proFormOpen) {
-        this.setState({
-          proFormOpen: false
-        });
-      }
-      this.setState({
-        genFormOpen: true
-      });
-    }
-    hideGenForm() {
-      this.setState({
-        genFormOpen: false
-      });
-    }
+//     showGenForm() {
+//       //if other form is open, close it first
+//       if (this.state.proFormOpen) {
+//         this.setState({
+//           proFormOpen: false
+//         });
+//       }
+//       this.setState({
+//         genFormOpen: true
+//       });
+//     }
+//     hideGenForm() {
+//       this.setState({
+//         genFormOpen: false
+//       });
+//     }
   
-    showProjectForm() {
-      if (this.state.genFormOpen) {
-        this.setState({
-          genFormOpen: false
-        });
-      }
-      this.setState({
-        proFormOpen: true
-      });
-    }
-    hideProjectForm() {
-      this.setState({
-        proFormOpen: false
-      });
-    }
+//     showProjectForm() {
+//       if (this.state.genFormOpen) {
+//         this.setState({
+//           genFormOpen: false
+//         });
+//       }
+//       this.setState({
+//         proFormOpen: true
+//       });
+//     }
+//     hideProjectForm() {
+//       this.setState({
+//         proFormOpen: false
+//       });
+//     }
   
-    render() {
-      return (
-        <Layout
-          pageTitle="Contact - "
-          headline="Let's chat"
-          className="contactPage"
-          backgroundIsBlack={false}
-        >
-          <Body>
-            <FormsHeader>
-              <FormH2>I can't wait to hear from you!</FormH2>
-              <FormH3>So, what do you need?</FormH3>
-              <FormBtnContainer>
-                <FormButton
-                  id="genBtn"
-                  onClick={
-                    this.state.genFormOpen ? this.hideGenForm : this.showGenForm
-                  }
-                >
-                  Question or Comment
-                </FormButton>
-                <FormButton
-                  id="protBtn"
-                  onClick={
-                    this.state.proFormOpen
-                      ? this.hideProjectForm
-                      : this.showProjectForm
-                  }
-                >
-                  I need a website built!
-                </FormButton>
-              </FormBtnContainer>
-            </FormsHeader>
-            <GeneralForm open={this.state.genFormOpen} />
-            <ProjectForm open={this.state.proFormOpen} />
-          </Body>
-        </Layout>
-      );
-    }
-  }
-  export default Contact;
+//     render() {
+//       return (
+//         <Layout
+//           pageTitle="Contact - "
+//           headline="Let's chat"
+//           className="contactPage"
+//           backgroundIsBlack={false}
+//         >
+//           <Body>
+//             <FormsHeader>
+//               <FormH2>I can't wait to hear from you!</FormH2>
+//               <FormH3>So, what do you need?</FormH3>
+//               <FormBtnContainer>
+//                 <FormButton
+//                   id="genBtn"
+//                   onClick={
+//                     this.state.genFormOpen ? this.hideGenForm : this.showGenForm
+//                   }
+//                 >
+//                   Question or Comment
+//                 </FormButton>
+//                 <FormButton
+//                   id="protBtn"
+//                   onClick={
+//                     this.state.proFormOpen
+//                       ? this.hideProjectForm
+//                       : this.showProjectForm
+//                   }
+//                 >
+//                   I need a website built!
+//                 </FormButton>
+//               </FormBtnContainer>
+//             </FormsHeader>
+//             <GeneralForm open={this.state.genFormOpen} />
+//             <ProjectForm open={this.state.proFormOpen} />
+//           </Body>
+//         </Layout>
+//       );
+//     }
+//   }
+//   export default Contact;
 
