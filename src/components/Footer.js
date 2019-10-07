8f9f887b36c533f93e52a5d360c389e71e60e1fb
Thomas Roman <thomasroman32@yahoo.com>
2019-10-06 23:53:49 -0500

    commenting out imports that are giving warning messages in Footer.js component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Link from 'gatsby-link'
+// import Link from 'gatsby-link'
 import styled from 'styled-components'
-import { EDEADLK } from 'constants';
+// import { EDEADLK } from 'constants';
 
 const FooterContainer = styled.footer`
     text-align: center;
