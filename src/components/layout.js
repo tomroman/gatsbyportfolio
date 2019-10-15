9acfedbcd7a347b8420cd2f86758a89e4cfcc170
Thomas Roman <thomasroman32@yahoo.com>
2019-10-14 23:43:24 -0500

    deleted the general form component code. May remove file later on if I cannot find a way to include the general form

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <Header siteTitle={data.site.siteMetadata.title} />
         <Menu /> 
         
         <div
