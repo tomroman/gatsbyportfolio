3d84cbb276b7718782077429f172a118cb14ba7d
Thomas Roman <thomasroman32@yahoo.com>
2019-10-29 22:42:18 -0500

    centered the name of the project in the project.js file

--- src/components/project.js
+++ src/components/project.js
@@ -2,22 +2,31 @@ import React from 'react';
 import { Link } from 'gatsby' 
 import Image from 'gatsby-image'
 
+
 const Project = ({title, description, url, imageData}) => (
 
-    <div className="project"> 
+    <div className="project" >
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+   
         <h1>{title}</h1>
+        
+        </div>
         <Image 
         
         fluid={imageData} alt={title} />
         <p>{description}</p>
+        <div style={{display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
         <p>
             <a href={url}> View this project online &rarr;</a>
            
         </p>
+        </div>
 
+       
         <p>
             <Link to= "/">&larr; back to all projects </Link>
         </p>
+        
 
     </div>
 )
